89feeab7e14f729dcff70b5ac6360f52640dc299
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-05-27 18:26:54 +0200

    normed here_doc.h

--- include/parsing/here_doc.h
+++ include/parsing/here_doc.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asinsard <asinsard@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: oelleaum <oelleaum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 23:51:21 by asinsard          #+#    #+#             */
-/*   Updated: 2025/05/27 17:12:29 by asinsard         ###   ########lyon.fr   */
+/*   Updated: 2025/05/27 18:26:39 by oelleaum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 # define NAME_LENGTH 10
 
-# include <stdbool.h>
 # include "structs.h"
+# include <stdbool.h>
 
 struct					s_token;
 typedef struct s_token	t_token;
@@ -28,7 +28,8 @@ bool					extract_stdin(int fd, char *limiter);
 bool					handle_here_doc(t_token **head, t_lists *lists);
 
 /* -------------------HERE DOC UTILS------------------- */
-bool					case_is_limiter(char *line, char *limiter, int fd, int len);
+bool					case_is_limiter(char *line, char *limiter, int fd,
+							int len);
 bool					free_lim_close_fd(char *limiter, int fd);
 bool					verif_here_doc(t_token **head);
 bool					verif_name(char *name);
