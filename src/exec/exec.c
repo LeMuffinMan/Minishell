9746847ec09f1c90966a7dfea8d4977a0f54133a
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-06-03 15:38:21 +0200

    space exit_code fixed

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -71,7 +71,7 @@ int	exec_ast(t_tree **ast, t_lists *lists)
 	struct sigaction	sa_orig;
 
 	if (!*ast)
-		return (127);
+		return (0);
 	sigemptyset(&sa_ignore.sa_mask);
 	sa_ignore.sa_handler = SIG_IGN;
 	sa_ignore.sa_flags = 0;
